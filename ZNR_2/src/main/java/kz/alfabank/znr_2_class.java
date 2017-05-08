ade4af56c2074b76463147f7531b74dda61c9fde
Sergey Shapov <a777b666@mail.ru>
2017-05-08 16:33:19 +0600

    ZNR_2_code commit

---
+++ ZNR_2/src/main/java/kz/alfabank/znr_2_class.java
@@ -0,0 +1,12 @@
+package kz.alfabank;
+
+/**
+ * Created by Sergey on 08.05.2017.
+ */
+public class znr_2_class
+{
+    public static void Say_hello_world(String name)
+    {
+        System.out.println("Hello "+name);
+    }
+}
