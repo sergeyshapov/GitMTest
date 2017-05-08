b4e484a35adf00ad163335f559750ba1aed8fba3
Sergey Shapov <a777b666@mail.ru>
2017-05-08 16:23:33 +0600

    ZNR1-first commit

---
+++ src/main/java/kz/alfabank/test11.java
@@ -0,0 +1,12 @@
+package kz.alfabank;
+
+/**
+ * Created by Sergey on 08.05.2017.
+ */
+public class test11
+{
+    public static void SayInfo()
+    {
+        System.out.println("AAAA1");
+    }
+}
