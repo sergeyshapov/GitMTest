61af3571ca317d9f8d3bdb0bafc7e738516df271
Sergey Shapov <a777b666@mail.ru>
2017-05-08 16:21:09 +0600

    Initial commit

---
+++ src/main/java/kz/alfabank/Main.java
@@ -0,0 +1,12 @@
+package kz.alfabank;
+
+/**
+ * Created by Sergey on 08.05.2017.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world 1");
+    }
+}
